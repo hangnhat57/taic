b5d9eb0361441d7a14487944c2b7c9c5a1a8269a
Nhat Nguyen <hangnhat57@gmail.com>
2016-10-03 22:14:32 +0700

    Make it perfect

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem "active_admin_import" , github: "activeadmin-plugins/active_admin_import"
 gem 'activeadmin', github: 'activeadmin'
 gem 'inherited_resources', github: 'activeadmin/inherited_resources'
 gem 'devise'
-gem "active_skin"
+gem 'active_admin_theme'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use sqlite3 as the database for Active Record
--- app/client/summaries.rb
+++ app/client/summaries.rb
@@ -1,4 +1,23 @@
 ActiveAdmin.register Summary,namespace: :client do
+config.clear_action_items!
+scope_to :current_customer
+index do
+  selectable_column
+  column :Product
+  column :Version
+  column :Amount
+  column :Username
+  column :Hostname
+  column   "Start Time",:StartTime
+  column   "End Time",:EndTime
+  column   "Duration by hours",:Duration_hours
+  column   "Duration by seconds",:Duration_second
 
+end
+
+
+filter :Product
+filter :StartTime
+filter :EndTime
 
 end
